3c2349d993d7c8cb55467ae4ae32f798e9e9c007
Morgován Nimród <morgovannimrod22@gmail.com>
2022-09-22 11:04:57 +0200

    2. Feladat elkezdése - Festmenyek osztály létrehozása

--- src/hu/petrik/festmenyoop/AukcioProjekt.java
+++ src/hu/petrik/festmenyoop/AukcioProjekt.java
@@ -1,5 +1,7 @@
 package hu.petrik.festmenyoop;
 
+import java.io.IOException;
+
 public class AukcioProjekt {
     public static void main(String[] args) {
     // test
@@ -9,5 +11,19 @@ public class AukcioProjekt {
         festmeny1.licit(25);
         festmeny1.licit();
         System.out.println(festmeny1);
+
+        //2.feladat
+        Festmenyek festmenyek = new Festmenyek();
+        festmenyek.addFestmeny(festmeny1);
+        Festmeny festmeny2 = new Festmeny("Java óra","Morgován Nimród","Futurizmus");
+        festmenyek.addFestmeny(festmeny2);
+        festmenyek.addFestmenyFelhasznalo();
+        try {
+            festmenyek.beolvasas("festmenyek.csv");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        festmenyek.licitXAlkalommal(20);
+
     }
 }
---
+++ src/hu/petrik/festmenyoop/Festmenyek.java
@@ -0,0 +1,63 @@
+package hu.petrik.festmenyoop;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Festmenyek {
+    private List<Festmeny> festmenyek;
+
+    public Festmenyek() {
+        this.festmenyek = new ArrayList<>();
+    }
+
+    public void addFestmeny(Festmeny festmeny) {
+        this.festmenyek.add(festmeny);
+    }
+
+    void addFestmenyFelhasznalo() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Add meg, hogy hány darab képet szeretnél a listába rakni:");
+        int darabszam = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < darabszam; i++) {
+            System.out.println("Add meg a kép nevét:");
+            String nev = sc.nextLine();
+            System.out.println("Add meg a festő nevét:");
+            String festo = sc.nextLine();
+            System.out.println("Add meg a kép stílusát:");
+            String stilus = sc.nextLine();
+            Festmeny festmeny = new Festmeny(nev, festo, stilus);
+            this.addFestmeny(festmeny);
+            System.out.println("\n\n");
+        }
+    }
+
+    public void beolvasas(String filenev) throws IOException {
+        FileReader fr = new FileReader(filenev);
+        BufferedReader br = new BufferedReader(fr);
+        String sor = br.readLine();
+        while (sor != null && !sor.equals("")) {
+            String[] adatok = sor.split(";");
+            Festmeny festmeny = new Festmeny(adatok[0], adatok[1], adatok[2]);
+            this.addFestmeny(festmeny);
+            sor = br.readLine();
+        }
+        fr.close();
+        br.close();
+    }
+    public void licitXAlkalommal(int x){
+        Random r = new Random();
+            for (Festmeny f : this.festmenyek) {
+                for (int i = 0; i < 20; i++) {
+                    f.licit((r.nextInt(101-10)+10));
+                }
+            }
+
+    }
+}
